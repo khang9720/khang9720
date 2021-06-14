51efbb864d7aa21bf3a421ad7e91fb33450b6328
nguyenminhkhang9720 <nguyenminhkhang9720@gmail.com>
2021-06-14 08:43:15 +0700

    test5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NZ_I18N } from 'ng-zorro-antd/i18n'
+import { en_US } from 'ng-zorro-antd/i18n'
+import { registerLocaleData } from '@angular/common'
+import en from '@angular/common/locales/en'
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NzMenuModule } from 'ng-zorro-antd/menu'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip'
+import { NzButtonModule } from 'ng-zorro-antd/button'
+
+registerLocaleData(en)
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzMenuModule,
+    NzIconModule,
+    NzToolTipModule,
+    NzButtonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
